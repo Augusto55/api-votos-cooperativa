07a62c2eea136757c8bf8d0c084ecb30bacfda48
Augusto55 <85200864+Augusto55@users.noreply.github.com>
2023-06-12 11:42:27 -0300

    Getters and setters

--- src/main/java/br/com/meta/apivotoscooperativa/model/SessaoVotacao.java
+++ src/main/java/br/com/meta/apivotoscooperativa/model/SessaoVotacao.java
@@ -12,4 +12,55 @@ public class SessaoVotacao {
     private Integer votosSim;
     private Integer votosNao;
     private String expireTime;
+
+    public SessaoVotacao() {
+    }
+
+    public SessaoVotacao(Integer id, Integer votosTotal, Integer votosSim, Integer votosNao, String expireTime) {
+        this.id = id;
+        this.votosTotal = votosTotal;
+        this.votosSim = votosSim;
+        this.votosNao = votosNao;
+        this.expireTime = expireTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVotosTotal() {
+        return votosTotal;
+    }
+
+    public void setVotosTotal(Integer votosTotal) {
+        this.votosTotal = votosTotal;
+    }
+
+    public Integer getVotosSim() {
+        return votosSim;
+    }
+
+    public void setVotosSim(Integer votosSim) {
+        this.votosSim = votosSim;
+    }
+
+    public Integer getVotosNao() {
+        return votosNao;
+    }
+
+    public void setVotosNao(Integer votosNao) {
+        this.votosNao = votosNao;
+    }
+
+    public String getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(String expireTime) {
+        this.expireTime = expireTime;
+    }
 }
